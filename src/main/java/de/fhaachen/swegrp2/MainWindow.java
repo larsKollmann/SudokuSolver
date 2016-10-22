729bdf418392223e01542953efa10adbf6f9f105
Sebastian Stollenwerk <Sebastian.Stollenwerk@alumni.fh-aachen.de>
2016-10-22 22:55:09 +0200

    Adding pom.xml for maven
    In this first step I added the pom.xml file, to be able to build the
    software easily. Adding a main class and the maven plugin to build the jar
    file.
    
    Also .gitignore was added to ignore stuff like the target folder and so
    on.

---
+++ src/main/java/de/fhaachen/swegrp2/MainWindow.java
@@ -0,0 +1,12 @@
+package de.fhaachen.swegrp2;
+
+/**
+ * This is the main class, where the first gui will be generated.
+ *
+ * TODO: Import javaFX and create some guis (@Kaan @Furkan)
+ */
+public class MainWindow {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
