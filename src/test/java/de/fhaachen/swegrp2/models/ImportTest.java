1041f4c9a203e536ddac558ce9b72ba0b9fac0c6
simonvdd <simonvdd@gmx.de>
2016-11-21 08:17:15 +0100

    JUnit Tests für Import

---
+++ src/test/java/de/fhaachen/swegrp2/models/ImportTest.java
@@ -0,0 +1,51 @@
+package de.fhaachen.swegrp2.models;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by simon on 21.11.2016.
+ */
+public class ImportTest {
+    Import testimporter;
+
+    @Before
+    public void setUp() throws Exception {
+        testimporter = new Import();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void importXMLTest() throws Exception {
+        // Reihe in Testdatei startet mit 0
+        int[][] testArray = testimporter.importXML("src/test/java/test.xml");
+
+        assertEquals(3, testArray[0][3]);
+        assertEquals(7, testArray[3][7]);
+        assertNotEquals(5, testArray[6][8]);
+    }
+
+    @Test
+    public void importCSVTest() throws Exception {
+        // Reihe in Testdatei startet mit 1
+        int[][] testArray = testimporter.importCSV("src/test/java/test.csv");
+
+        assertEquals(3, testArray[0][2]);
+        assertEquals(7, testArray[3][6]);
+        assertNotEquals(5, testArray[6][8]);
+    }
+
+    @Test
+    public void importJSONTest() throws Exception {
+        int[][] testArray = testimporter.importJSON("src/test/java/test.json");
+
+        assertEquals(3, testArray[0][2]);
+        assertEquals(7, testArray[3][5]);
+        assertNotEquals(5, testArray[6][8]);
+    }
+
+}
