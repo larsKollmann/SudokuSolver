5ab9e8be3deef4c9bd59a4a0bb27bba3b91b987a
simonvdd <simonvdd@gmx.de>
2016-12-12 13:37:27 +0100

    Einige Änderungen zum Testen der GUI, größtenteils mit Methodenaufrufen, für Methoden, die in anderen Branches sind bzw. noch garnicht existieren

--- src/main/java/de/fhaachen/swegrp2/controllers/GUIControllers/SudokuSceneController.java
+++ src/main/java/de/fhaachen/swegrp2/controllers/GUIControllers/SudokuSceneController.java
@@ -1,9 +1,9 @@
 package de.fhaachen.swegrp2.controllers.GUIControllers;
 
+import de.fhaachen.swegrp2.controllers.SudokuController;
 import de.fhaachen.swegrp2.controllers.SudokuField;
 import de.fhaachen.swegrp2.models.Import;
-import de.fhaachen.swegrp2.models.solver.SudokuGrid;
-import javafx.beans.binding.BooleanBinding;
+//import de.fhaachen.swegrp2.models.solver.SudokuGrid;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -15,29 +15,57 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 
+import static java.lang.Math.sqrt;
 import static javafx.scene.layout.Priority.*;
 
 public class SudokuSceneController
 {
-    private static final Logger log = LoggerFactory.getLogger(SudokuSceneController.class);
+    private SudokuController control = SudokuController.getInstance();
 
-    // Größe des zu zeichnenden Sudokus
-    private static int size = 4; // SudokuController.getInstance().getSudokuField().getSize();
-    private static SudokuField field = new SudokuField(size*size);
+    private static class GuiPropertiesBySize{
+        int subFieldSize;
+        int mainSize;
+
+        int fontSize;
+
+        int stageMinHeight;
+        int stageMinWidth;
+
+        GuiPropertiesBySize(int subFieldSize, int fontSize, int stageMinHeight, int stageMinWidth) {
+            this.subFieldSize = subFieldSize;
+            this.mainSize = subFieldSize*subFieldSize;
+            this.fontSize = fontSize;
+            this.stageMinHeight = stageMinHeight;
+            this.stageMinWidth = stageMinWidth;
+        }
+    }
+
+    static GuiPropertiesBySize[] guiprop = new GuiPropertiesBySize[] {
+            new GuiPropertiesBySize(3, 22, 495, 415),
+            new GuiPropertiesBySize(4, 20, 615, 540),
+            new GuiPropertiesBySize(5, 17, 765, 688),
+            new GuiPropertiesBySize(6, 14, 910, 830)
+    };
 
     @FXML private GridPane mainGridPane;
 
     @FXML
     protected void initialize() {
-        addrowcolumnconstraints(mainGridPane);
         mainGridPane.minWidthProperty().bind(mainGridPane.heightProperty());
         mainGridPane.prefWidthProperty().bind(mainGridPane.heightProperty());
         mainGridPane.maxWidthProperty().bind(mainGridPane.heightProperty());
+
+        drawGrid();
+    }
+
+    private void drawGrid() {
+        int size = control.getSubFieldsize();
+
+        addrowcolumnconstraints(mainGridPane);
         // SudokuFeld gewünschter Größe in Scene einfügen
         for(int xl = 0; xl < size; xl++){
             for(int yl = 0; yl < size; yl ++){
@@ -49,7 +77,7 @@ public class SudokuSceneController
 
                 for(int xs = 0; xs < size; xs++) {
                     for (int ys = 0; ys < size; ys++) {
-                        int[] coords = translateGridCoordstoXY(xl,yl,xs,ys);
+                        int[] coords = translateGridCoordstoXY(xl,yl,xs,ys,size);
 
                         Pane pane = new Pane();
                         pane.setStyle("-fx-background-color: white");
@@ -83,7 +111,21 @@ public class SudokuSceneController
         }
     }
 
+    private void redrawGrid() throws IOException {
+        mainGridPane.getChildren().clear();
+        mainGridPane.getColumnConstraints().clear();
+        mainGridPane.getRowConstraints().clear();
+
+        drawGrid();
+//
+//        Scene scene = mainGridPane.getScene();
+//        Stage stage = (Stage) scene.getWindow();
+//        stage.setMinHeight(152 * size);
+//        stage.setMinWidth(139 * size);
+    }
+
     private void addrowcolumnconstraints(GridPane gridPane) {
+        int size = control.getSubFieldsize();
         for(int i = 0; i < size; i++) {
             ColumnConstraints col = new ColumnConstraints();
             col.setHgrow(ALWAYS);
@@ -96,7 +138,7 @@ public class SudokuSceneController
         }
     }
 
-    private static int[] translateXYToGridCoords(int x, int y) {
+    private static int[] translateXYToGridCoords(int x, int y, int size) {
         int[] ret = new int[4];
 
         ret[0] = x/size;
@@ -107,23 +149,23 @@ public class SudokuSceneController
         return ret;
     }
 
-    private static int[] translateGridCoordstoXY(int xl, int yl, int xs, int ys) {
+    private static int[] translateGridCoordstoXY(int xl, int yl, int xs, int ys, int size) {
         int x = xl * size + xs;
         int y = yl * size + ys;
         return new int[]{x, y};
     }
 
-    private void fillWithCurrentSudokuField(SudokuField field) {
-        fillWithCurrentSudokuField(field, Color.BLACK);
+    private void fillWithCurrentSudokuField() {
+        fillWithCurrentSudokuField(Color.BLACK);
     }
 
-    private void fillWithCurrentSudokuField(SudokuField field, Color color) {
-        int dim = size * size;
+    private void fillWithCurrentSudokuField(Color color) {
+        int dim = control.getSize();
 
         for(int x = 0; x < dim; x++) {
             for(int y = 0; y < dim; y++) {
                 Text text = (Text) mainGridPane.lookup("#Text" + x + "," + y);
-                int number = field.getFieldValue(y, x);
+                int number = control.getFieldValue(y, x);
                 if (!text.getText().equals(number + "")) {
                     text.setFill(color);
                     if (number != 0)
@@ -136,52 +178,69 @@ public class SudokuSceneController
     }
 
     @FXML
-    public void myimport(Event event) {
-        Import importer = new Import();
+    public void changeSize(ActionEvent actionEvent) throws IOException {
+        String sourceID = actionEvent.getSource().toString();
+        int size = Integer.parseInt(sourceID.substring(sourceID.indexOf('=') + 10, sourceID.indexOf(',')));
 
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("CSV-Datei importieren");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV-Dateien", "*.csv"));
+        control.reset(size*size);
+        redrawGrid();
+    }
 
-        File file = fileChooser.showOpenDialog((Stage)mainGridPane.getScene().getWindow());
-        try {
-            field = importer.importCSV(file.getPath());
-            fillWithCurrentSudokuField(field, Color.BLUE);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//    @FXML
+//    public void solve(Event event) {
+//        SudokuGrid grid = SudokuGrid.getGrid(field.getSudokuField(), size);
+//
+//        if(grid.solve()) {
+//            try {
+//                field = new SudokuField(grid.getGridAsIntArr());
+//                fillWithCurrentSudokuField(field);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        else {
+//            System.out.println("Not solvable");
+//        }
+//    }
+
+    @FXML
+    public void importFile(ActionEvent actionEvent) {
+//
+//        String sourceID = actionEvent.getSource().toString();
+//        String fileType = sourceID.substring(sourceID.indexOf('$') + 1, sourceID.indexOf(','));
+//
+//        FileChooser fileChooser = new FileChooser();
+//        fileChooser.setTitle(fileType + "-Datei importieren");
+//        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(fileType + "-Dateien", "*." + fileType.toLowerCase()));
+//
+//        File file = fileChooser.showOpenDialog((Stage)mainGridPane.getScene().getWindow());
+//        try {
+//            field = control.importFile(file);
+//
+//            redrawGrid(control.getSubFieldsize());
+//            fillWithCurrentSudokuField(field, Color.BLUE);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
     @FXML
-    public void solve(Event event) {
-        SudokuGrid grid = SudokuGrid.getGrid(field.getSudokuField(), size);
-
-        if(grid.solve()) {
-            try {
-                field = new SudokuField(grid.getGridAsIntArr());
-                fillWithCurrentSudokuField(field);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            System.out.println("Not solvable");
-        }
+    public void exportFile(ActionEvent actionEvent) {
     }
 
-    public void importjson(ActionEvent actionEvent) {
-        Import importer = new Import();
-
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("JSON-Datei importieren");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON-Dateien", "*.json"));
-
-        File file = fileChooser.showOpenDialog((Stage)mainGridPane.getScene().getWindow());
-        try {
-            field = importer.importJSON(file.getPath());
-            fillWithCurrentSudokuField(field, Color.BLUE);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @FXML
+    public void exportPDF(ActionEvent actionEvent) {
     }
+
+    @FXML
+    /*DEBUG*/ public void printGuiValues(ActionEvent actionEvent) {
+        Stage stage = (Stage)mainGridPane.getScene().getWindow();
+        System.out.println(stage);
+        System.out.println("W: " + stage.getWidth() + " H: " + stage.getHeight());
+    }
+
+
+    public void generate(ActionEvent actionEvent) {
+    }
+
 }
