6099ebdb0ae16c33946d54f345b208d73feb84f0
Lars Kollmann <larsko1994@gmail.com>
2016-12-19 17:39:11 +0100

    Hilfetexte werden nun per JSON eingelesen und könnne über den HelpController als Liste angefordert werden

---
+++ src/main/java/de/fhaachen/swegrp2/models/Help.java
@@ -0,0 +1,16 @@
+package de.fhaachen.swegrp2.models;
+
+/**
+ * Created by lars on 19.12.16.
+ */
+public class Help {
+
+    public String titel;
+    public String text;
+
+    public Help(String titel, String text) {
+        this.text = text;
+        this.titel = titel;
+    }
+
+}
