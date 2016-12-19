3c2bc0cde2391ab13755a63eb0ab4e5be6f435a0
Berat Ertural <beertural@gmail.com>
2016-12-19 17:03:10 +0100

    Export Funktionen überarbeitet

--- src/main/java/de/fhaachen/swegrp2/controllers/SudokuController.java
+++ src/main/java/de/fhaachen/swegrp2/controllers/SudokuController.java
@@ -33,37 +33,4 @@ public class SudokuController {
     public static boolean isSizeSupported(int size) {
         return (size == 9 || size == 16 || size == 25 || size == 36);
     }
-
-
-    /**Beispiel Dummy funktion damit sich alle vorstellen können was die Aufgabe des SudokuControllers ist
-     * weitere Funktionen werden erst implementiert wenn diese benoetigt werden
-     *
-     * Extrahiert die Dateiendung von dem Datenpfad und ruft die entsprechende Import funktion auf
-    *Das ergebnis wird anschließend in dem internen SudokuField gespeichert
-    *Aufgerufen von der GUI Explicit von einem JavaFX FileChooser Objekt
-    *Exceptions sollten in der oberen ebende (GUI) gefangen uns als fehlermedlung dem user angezeigt werden
-    public void ImportFile(File file) throws Exception{
-        String extension = "";
-        String filePath = file.getPath();
-        int i = filePath.lastIndexOf('.');
-        if (i > 0) {
-            extension = filePath.substring(i+1);
-            if (extension.equals("xml")) {
-                field = new SudokuField(importer.importXML(filePath).getField());
-            }
-            else if (extension.equals("csv")) {
-                field = new SudokuField(importer.importCSV(filePath).getField());
-            }
-            else if (extension.equals("json")) {
-                field = new SudokuField(importer.importJSON(filePath).getField());
-            }
-            else {
-                //Fehlermeldung
-            }
-        }
-        else {
-            //Fehlermeldung
-        }
-    }
-     */
 }
--- src/main/java/de/fhaachen/swegrp2/models/Export.java
+++ src/main/java/de/fhaachen/swegrp2/models/Export.java
@@ -11,8 +11,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 
-
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -21,94 +19,125 @@ import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-/**
- * Created by lars on 17.11.16.
+import static de.fhaachen.swegrp2.controllers.SudokuController.isSizeSupported;
+
+/**<p><b>Titel:</b> Export</p>
+ * <p><b>Beschreibung:</b> Bietet Methoden zum export gegebener Sudokus im 2D int Array Format
+ * mit gegeben Dateipfad in diverse XML, CSV, JSON und PDF.</p>
  */
 public class Export {
+    public Export () {}
 
-	public static void ExportXML(int size, int wert[][], String path) {
-		try {
+    /**
+     * Exportiert ein gegebenes Sudoku ins XML-Format
+     * @param arr Das Sudoku im 2D int Array Format
+     * @param path Der Dateipfad zu dem exportiert werden soll
+     * @throws Exception Warnung vor fehlerhaften Sudoku Array
+     */
+    public void exportXML(int arr[][], String path) throws Exception {
+        try {
+            int size = arr.length;
+            if (!isSizeSupported(size))
+                throw new ExceptionSuite.SizeNotSupportedException("Nicht unterstützte Groeße!");
 
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
-			// root elements
-			Document doc = docBuilder.newDocument();
-			Element rootElement = doc.createElement("Sudoku");
-			doc.appendChild(rootElement);
-			Attr attr = doc.createAttribute("size");
-			attr.setValue(String.valueOf(size));
-			rootElement.setAttributeNode(attr);
+            // root elements
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("Sudoku");
+            doc.appendChild(rootElement);
+            Attr attr = doc.createAttribute("size");
+            attr.setValue(String.valueOf(size));
+            rootElement.setAttributeNode(attr);
 
-			for (int i = 0; i < size; i++) {
-				// row elements
-				Element row = doc.createElement("row");
-				rootElement.appendChild(row);
+            for (int i = 0; i < size; i++) {
+                // row elements
+                Element row = doc.createElement("row");
+                rootElement.appendChild(row);
 
-				for (int y = 0; y < size; y++) {
-					// column elements
-					Element col = doc.createElement("col");
-					if (wert[i][y] > 0 && wert[i][y] < size)
-						col.appendChild(doc.createTextNode(String.valueOf(wert[i][y])));
-					row.appendChild(col);
-				}
+                for (int y = 0; y < size; y++) {
+                    // column elements
+                    Element col = doc.createElement("col");
+                    if (arr[i][y] > 0 && arr[i][y] < size)
+                        col.appendChild(doc.createTextNode(String.valueOf(arr[i][y])));
+                    row.appendChild(col);
+                }
 
-			}
+            }
 
-			// write the content into xml file
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File(path));
-			
-			transformer.transform(source, result);
-			
-		} catch (ParserConfigurationException pce) {
-			pce.printStackTrace();
-		} catch (TransformerException tfe) {
-			tfe.printStackTrace();
-		}
-	}
-	
-	
-    public static void ExportCSV(int wert[][]) throws Exception {
-    StringBuilder builder = new StringBuilder();
-    for (int i = 0; i < wert.length; i++) {
-      for (int j = 0; j < wert.length; j++) {
-        builder.append(wert[i][j] + "");
-        if (j < wert.length - 1)
-          builder.append(",");
-      }
-      builder.append("\n");
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(path));
+
+            transformer.transform(source, result);
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        }
     }
-    BufferedWriter writer = new BufferedWriter(new FileWriter("export.csv"));
-    writer.write(builder.toString());
-    writer.close();
-  }
 
-    
-    public static void ExportJSON(int wert[][]) throws IOException{
-		StringBuilder builder = new StringBuilder();
-		builder.append("{\r\n");
-		builder.append("  \"size\": " + wert.length + ",\r\n");
-		builder.append("  \"sudoku\": [\r\n");
-		
-		for(int i = 0; i < wert.length; i++){
-			builder.append("    ");
-			for(int j = 0; j < wert.length; j++){
-				builder.append(wert[i][j]);
-				if(!(i== wert.length-1 && j== wert.length-1)){
-					builder.append(",");
-				}
-			}
-			builder.append("\r\n");
-		}
-		builder.append("  ]\r\n");
-		builder.append("}");
-		
-		BufferedWriter writer = new BufferedWriter(new FileWriter("sudoku.json"));     
-		writer.write(builder.toString());     
-		writer.close();
-	}
-    
+    /**
+     * Exportiert ein gegebenes Sudoku ins CSV-Format
+     * @param arr Das Sudoku im 2D int Array Format
+     * @param path Der Dateipfad zu dem exportiert werden soll
+     * @throws Exception Warnung vor fehlerhaften Sudoku Array sowie IOExcepton
+     */
+    public void exportCSV(int arr[][], String path) throws Exception {
+        int size = arr.length;
+        if (!isSizeSupported(size))
+            throw new ExceptionSuite.SizeNotSupportedException("Nicht unterstützte Groeße!");
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                builder.append(arr[i][j] + "");
+                if (j < size - 1)
+                    builder.append(",");
+            }
+            builder.append("\n");
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        writer.write(builder.toString());
+        writer.close();
+    }
+
+    /**
+     * Exportiert ein gegebenes Sudoku ins JSON-Format
+     * @param arr Das Sudoku im 2D int Array Format
+     * @param path Der Dateipfad zu dem exportiert werden soll
+     * @throws Exception Warnung vor fehlerhaften Sudoku Array sowie IOException
+     */
+    public void exportJSON(int arr[][], String path) throws Exception {
+        int size = arr.length;
+        if (!isSizeSupported(size))
+            throw new ExceptionSuite.SizeNotSupportedException("Nicht unterstützte Groeße!");
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("{\r\n");
+        builder.append("  \"size\": " + size + ",\r\n");
+        builder.append("  \"sudoku\": [\r\n");
+
+        for (int i = 0; i < size; i++) {
+            builder.append("    ");
+            for (int j = 0; j < size; j++) {
+                builder.append(arr[i][j]);
+                if (!(i == size - 1 && j == arr.length - 1)) {
+                    builder.append(",");
+                }
+            }
+            builder.append("\r\n");
+        }
+        builder.append("  ]\r\n");
+        builder.append("}");
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        writer.write(builder.toString());
+        writer.close();
+    }
+
 }
